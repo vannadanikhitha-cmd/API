7dd70e1469270e50c35b21a2f8371b70890dbe95
vannadanikhitha <vannadanikhitha@gmail.com>
2026-06-04 12:40:36 +0530

    added connection test files

--- employee_mangement/database/connection.py
+++ employee_mangement/database/connection.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
-from config.settings import settings
+from urllib.parse import quote_plus
 
-DATABASE_URL = (
-    f"mssql+pyodbc://@{settings.DB_SERVER}/"
-    f"{settings.DB_NAME}"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
-    "&trusted_connection=yes"
+params = quote_plus(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=SIVA\\SQLEXPRESS;"
+    "DATABASE=employee_management;"
+    "Trusted_Connection=yes;"
+    "TrustServerCertificate=yes;"
 )
+
+DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={params}"
 
 engine = create_engine(
     DATABASE_URL,
